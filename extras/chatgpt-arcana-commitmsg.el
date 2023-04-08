e71f4213273ebb3bc6ff1f9c63376788a0e3e23e
Carl Lange <carl@flax.ie>
2023-04-08 23:27:49 +0100

    Use magit-toplevel to get the top of the repo
    This commit updates the `chatgpt-arcana-commitmsg` to use the `magit-toplevel` function to find the root directory of the Git repository, instead of relying on the assumption that the Emacs buffer is in the root directory.

--- extras/chatgpt-arcana-commitmsg.el
+++ extras/chatgpt-arcana-commitmsg.el
@@ -1,16 +1,18 @@
 (defun chatgpt-arcana-commitmsg--git-get-current-diff ()
   "Get the current diff from Git."
-  (shell-command-to-string "git diff HEAD"))
+  (let ((default-directory (magit-toplevel)))
+    (shell-command-to-string "git diff HEAD")))
 
 (defun chatgpt-arcana-commitmsg--git-get-last-few-commits (num-commits)
   "Get the last NUM-COMMITS commit messages from Git."
-  (let ((num-commits-str (number-to-string num-commits)))
+  (let ((num-commits-str (number-to-string num-commits))
+        (default-directory (magit-toplevel)))
     (shell-command-to-string (concat "git log -" num-commits-str " --format=%B-"))))
 
 (defcustom chatgpt-arcana-commitmsg-system-prompt
   "You are an advanced program used to generate commit messages.
 Your job is simple: generate a commit message based on the provided diff and previous commit messages.
-Read the diff closely!
+READ THE DIFF CLOSELY.
 
 Attempt to match the style and tone of previous commit messages as much as possible.
 The generated message MUST match the style and tone of the previous commit messages.
@@ -20,6 +22,7 @@ If they are descriptive, be descriptive; if they are concise, be concise; if the
 
 (defun chatgpt-arcana-commitmsg-query-api (prompt)
   "Query the API with the given PROMPT for a commit message."
+  (message "%S" prompt)
   (chatgpt-arcana--query-api-alist
    `(((role . "system") (content . ,chatgpt-arcana-commitmsg-system-prompt))
      ((role . "system") (name . "example_user")
@@ -44,9 +47,7 @@ Here are the latest commit messages:
 Create a sick new index page
 -
 Initialise my awesome new blog
--
-
-Match the style of these commit messages."))
+-"))
      ((role . "system") (name . "example_assistant")
       (content . "Update blog name to be more fun
 
@@ -61,7 +62,6 @@ Updated both the page title and the header."))
          (prompt (concat "Please generate a commit message based on the following diff:\n\n"
                          diff "\n\n"
                          "Here are the latest commit messages:\n"
-                         last-commits "\n\n"
-                         "Match the style of these commit messages."))
+                         last-commits))
          (commit-msg (chatgpt-arcana-commitmsg-query-api prompt)))
     (insert commit-msg)))
