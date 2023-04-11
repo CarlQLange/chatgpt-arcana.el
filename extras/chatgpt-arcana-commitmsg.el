acf7d760a0c9558714c59f8951a8b78589f48ecc
Carl Lange <carl@flax.ie>
2023-04-11 15:08:08 +0100

    Change how commits are fetched; update prompts
    Still not very happy with the results to be honest.

--- extras/chatgpt-arcana-commitmsg.el
+++ extras/chatgpt-arcana-commitmsg.el
@@ -9,58 +9,74 @@
         (default-directory (magit-toplevel)))
     (shell-command-to-string (concat "git log -" num-commits-str " --format=%B-"))))
 
+(defun chatgpt-arcana-commitmsg--git-get-last-diffs-and-commits (count)
+  "Get the last COUNT diffs and commits from Git as an alist."
+  (let* ((branch (magit-rev-parse "--abbrev-ref" "HEAD"))
+         (default-directory (magit-toplevel))
+         (log-command (concat "git log " branch " -" (number-to-string count) " -p --pretty=format:'COMMIT:%B%nDIFF:'")))
+    (let* ((output (concat (shell-command-to-string log-command) "COMMIT:"))
+           (commit-messages)
+           (diffs)
+           (matched-lines)
+           (in-commit)
+           (in-diff))
+      (dolist (line (split-string output "\n"))
+        (when (string-prefix-p "COMMIT:" line)
+          (when in-diff
+            (push (string-join (reverse matched-lines) "\n") diffs)
+            (setq matched-lines nil))
+          (setq in-commit t)
+          (setq in-diff nil))
+        (when (string-prefix-p "DIFF:" line)
+          (when in-commit
+            (push (string-join (reverse matched-lines) "\n") commit-messages)
+            (setq matched-lines nil))
+          (setq in-diff t)
+          (setq in-commit nil))
+        (push (replace-regexp-in-string "^\\(?:COMMIT\\|DIFF\\):" "" line) matched-lines))
+      (reverse (cl-mapcar #'list commit-messages diffs)))))
+
 (defcustom chatgpt-arcana-commitmsg-system-prompt
   "You are an advanced program used to generate commit messages.
 Your job is simple: generate a commit message based on the provided diff and previous commit messages.
-READ THE DIFF CLOSELY. Mention all noteworthy changes.
-
-Attempt to match the style and tone of previous commit messages as much as possible.
-The generated message MUST match the style and tone of the previous commit messages.
-If they are descriptive, be descriptive; if they are concise, be concise; if they are funny, be funny."
+It is crucial that you match the style and tone of previous commit messages as much as possible.
+Be concise."
   "System prompt for commit message generation"
   :type 'string :group 'chatgpt-arcana)
+
+(defun chatgpt-arcana--build-alist-from-history ()
+  (let* ((diffs-commits (chatgpt-arcana-commitmsg--git-get-last-diffs-and-commits 4))
+         (diffs (mapcar #'cadr diffs-commits))
+         (commits (mapcar #'car diffs-commits))
+         (last-commits (mapcar (lambda (commit) (nth 4 (split-string commit "\n"))) (cdr commits)))
+         (initial-commit (car commits)))
+    (list (list (cons 'role "system")
+                (cons 'name "example_user")
+                (cons 'content (concat "please generate a commit message based on the following diff: " (car diffs))))
+          (list (cons 'role "system")
+                (cons 'name "example_assistant")
+                (cons 'content initial-commit)))))
 
 (defun chatgpt-arcana-commitmsg-query-api (prompt)
   "Query the API with the given PROMPT for a commit message."
   (chatgpt-arcana--query-api-alist
    `(((role . "system") (content . ,chatgpt-arcana-commitmsg-system-prompt))
-     ((role . "system") (name . "example_user")
-      (content . "Please generate a commit message based on the following diff:
-
-diff --git a/index.html b/index.html
-index b8eac52..9348782 100644
---- a/index.html
-+++ b/index.html
-@@ -1,5 +1,5 @@
- <html>
--<head><title>Carl's Blog</title></head>
-+<head><title>Teh blog of doom!</title></head>
- <body>
--   <h1>My Blog</h1>
-+   <h1>Teh blog of doom!</h1>
-   <p>This is a sample HTML document.</p>
- </body>
- </html>
-
-Here are the latest commit messages:
-Create a sick new index page
--
-Initialise my awesome new blog
--"))
-     ((role . "system") (name . "example_assistant")
-      (content . "Update blog name to be more fun
-
-Updated both the page title and the header."))
+     ,@(chatgpt-arcana--build-alist-from-history)
      ((role . "user") (content . ,prompt)))))
 
 (defun chatgpt-arcana-commitmsg ()
   "Get a commit message based on the current diff and previous commit messages."
   (interactive)
   (let* ((diff (chatgpt-arcana-commitmsg--git-get-current-diff))
-         (last-commits (chatgpt-arcana-commitmsg--git-get-last-few-commits 10))
-         (prompt (concat "Please generate a commit message based on the following diff:\n\n"
-                         diff "\n\n"
-                         "Here are the latest commit messages:\n"
-                         last-commits))
+         (last-commits (chatgpt-arcana-commitmsg--git-get-last-few-commits 5))
+         (prompt (format "Please generate a commit message in keeping with the previous commit messages.
+Previous commit messages look like the following:
+------
+%s
+------
+Your message is about the following diff:
+%s"
+                         last-commits
+                         diff))
          (commit-msg (chatgpt-arcana-commitmsg-query-api prompt)))
     (insert commit-msg)))
